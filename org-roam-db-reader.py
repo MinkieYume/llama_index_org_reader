6a0b39e75ddd9a871ec458e22d070515c681be49
MinkieYume <minkieyume@gmail.com>
2025-05-09 17:12:38 +0800

    整理和提交格式

--- org-roam-db-reader.py
+++ org-roam-db-reader.py
@@ -4,28 +4,20 @@ import sqlite3
 import argparse
 
 class OrgRoamDbReader:
-    def    __init__(self, db_file, roam_path):
-        self.db_file      = db_file
-        self.roam_path     = roam_path
+    def    __init__(self):
+        self.db_file = Path.home()     /   "org-roam/org-roam.db"
+        self.roam_path = Path.home()     /   "org-roam"
 
-    @staticmethod
     def parse_args():
-        parser   = argparse.ArgumentParser(description="Org Roam Database Reader")
+        parser = argparse.ArgumentParser(description="Org Roam Database Reader")
         parser.add_argument("--db", help="Path to the Org Roam database file")
         parser.add_argument("--dir", help="Path to the directory containing Org Roam files")
-        args     = parser.parse_args()
-
-        return args
+        args = parser.parse_args()
+        if args.db:
+            self.db_file = args.db
+        if args.dir:
+            self.roam_path = args.dir
 
 if  __name__      ==   "__main__":
-    db_file         = Path.home()     /   "org-roam/org-roam.db"
-    roam_path       = Path.home()     /   "org-roam"
-
-    args            = OrgRoamDbReader.parse_args()
-
-    if args.db:
-        db_file     = args.db
-    if args.dir:
-        roam_path     = args.dir
-
-    reader          = OrgRoamDbReader(db_file, roam_path)
+    args = OrgRoamDbReader.parse_args()
+    reader = OrgRoamDbReader(db_file, roam_path)
