e2f3f291d320044dfe0288670380d22f966ccf1f
MinkieYume <minkieyume@gmail.com>
2025-05-09 16:38:39 +0800

    www

--- org-roam-db-reader.py
+++ org-roam-db-reader.py
@@ -7,18 +7,5 @@ class OrgRoamDbReader:
         self.db_file = db_file
         self.roam_path = roam_path
 
-    def get_db_path(self):
-        return Path(self.root).joinpath("org-roam").joinpath("org-roam.db")
-
-    def get_title_and_content(self, node_id):
-        db_path = self.get_db_path()
-        conn = sqlite3.connect(db_path)
-        cursor = conn.cursor()
-        cursor.execute("""SELECT title, content FROM nodes WHERE id = ?""", (node_id,))
-        result = cursor.fetchone()
-        conn.close()
-
-        return result
-
 if __name__ == "__main__":
     reader = OrgRoamDbReader()
